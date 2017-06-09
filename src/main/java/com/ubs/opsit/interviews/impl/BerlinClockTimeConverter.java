e5f93d860a57a25630f9a22610654d0e328339e7
v.kozachenco <v.kozachenko@persilab.com>
2017-06-09 22:20:24 +0300

    Initial commit

---
+++ src/main/java/com/ubs/opsit/interviews/impl/BerlinClockTimeConverter.java
@@ -0,0 +1,13 @@
+package com.ubs.opsit.interviews.impl;
+
+import com.ubs.opsit.interviews.TimeConverter;
+
+/**
+ * Created by kozak on 09.06.2017.
+ */
+public class BerlinClockTimeConverter implements TimeConverter {
+    @Override
+    public String convertTime(String aTime) {
+        return null;
+    }
+}
